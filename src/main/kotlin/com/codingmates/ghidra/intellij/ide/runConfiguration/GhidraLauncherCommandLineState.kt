2b68fd0cb60dcbe464b016c5da1503b678b37984
Uxinnn <xinyu.foong@gmail.com>
2025-12-29 16:49:30 +0800

    Modified ghidra launcher classpaths

--- src/main/kotlin/com/codingmates/ghidra/intellij/ide/runConfiguration/GhidraLauncherCommandLineState.kt
+++ src/main/kotlin/com/codingmates/ghidra/intellij/ide/runConfiguration/GhidraLauncherCommandLineState.kt
@@ -1,11 +1,15 @@
 package com.codingmates.ghidra.intellij.ide.runConfiguration
 
+import com.codingmates.ghidra.intellij.ide.facet.GhidraFacet
 import com.intellij.execution.ExecutionException
 import com.intellij.execution.application.BaseJavaApplicationCommandLineState
 import com.intellij.execution.configurations.JavaParameters
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.util.JavaParametersUtil
 import org.jetbrains.annotations.NotNull
+import kotlin.io.path.Path
+import kotlin.io.path.isRegularFile
+import kotlin.io.path.listDirectoryEntries
 
 
 class GhidraLauncherCommandLineState(
@@ -34,9 +38,19 @@ class GhidraLauncherCommandLineState(
         JavaParametersUtil.configureProject(
             project,
             javaParameters,
-            JavaParameters.JDK_AND_CLASSES_AND_TESTS,
+            JavaParameters.JDK_ONLY,
             jrePath
         )
+        val facet = GhidraFacet.findAnyInProject(project)
+        val ghidraHome = facet?.installationPath ?: throw ExecutionException(
+            "Unable to find Ghidra root directory. " +
+                    "Open the Ghidra facet and re-apply so all module libs are added."
+        )
+        javaParameters.classPath.add("$ghidraHome/Ghidra/Framework/Utility/lib/Utility.jar")
+        val regex = Regex("log4j-.*\\.jar$")
+        val s = Path("$ghidraHome/Ghidra/Framework/Generic/lib").listDirectoryEntries()
+            .filter { path -> path.isRegularFile() && regex.matches(path.fileName.toString()) }
+        s.forEach { path -> javaParameters.classPath.add(path.toAbsolutePath().toString()) }
         setupJavaParameters(javaParameters)
         val cp = javaParameters.classPath.pathList
         val hasUtility = cp.any { it.endsWith("/Framework/Utility/lib/Utility.jar") || it.endsWith("\\Framework\\Utility\\lib\\Utility.jar") }
@@ -53,7 +67,21 @@ class GhidraLauncherCommandLineState(
 
     companion object {
         private val GHIDRA_CLI_OPTS = listOf(
-            "-Djava.system.class.loader=ghidra.GhidraClassLoader"
+            "-Djava.system.class.loader=ghidra.GhidraClassLoader",
+            "-Dfile.encoding=UTF8",
+            "-Duser.country=US",
+            "-Duser.language=en",
+            "-Duser.variant=",
+            "-Dsun.java2d.opengl=false",
+            "-Djdk.tls.client.protocols=TLSv1.2,TLSv1.3",
+            "-Dcpu.core.limit=",
+            "-Dcpu.core.override=",
+            "-Dfont.size.override=",
+            "-Dpython.console.encoding=UTF-8",
+            "-Xshare:off",
+            "-Dsun.java2d.d3d=false",
+            "-Dlog4j.skipJansi=true",
+            "-XX:+ShowCodeDetailsInExceptionMessages",
         )
     }
 }
